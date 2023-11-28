9f9df06617930358cb32b4904e5eefdc67735a60
Raphael Collet <rco@odoo.com>
2023-11-28 17:23:18 +0100

    [IMP] manifest licence

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -20,6 +20,7 @@
     # for the full list
     'category': 'Test',
     'version': '0.1',
+    'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'board', 'website'],
